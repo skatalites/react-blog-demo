a769ece90f11fd2281e638aff5045d0e6a5d2922
skatalites <theskatalites@gmail.com>
2020-08-11 18:49:23 -0500

    Chages with async await

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
-import Count from './components/Count'
-
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
+import Count from "./components/Count";
 
 class Blog extends Component {
   constructor() {
     super();
-    this.state = { 
-      articles:[]
-      };
+    this.state = {
+      articles: []
+    };
   }
 
-  componentDidMount(){
-    let promesa = fetch('https://jsonplaceholder.typicode.com/posts');
+  async componentDidMount() {
+    let promesa = fetch("https://jsonplaceholder.typicode.com/posts");
 
+    //promesa un solo valor
     //Forma 1
     /*promesa.then( (response) => {
        response.json().then( (data) => {
@@ -24,32 +24,45 @@ class Blog extends Component {
     });*/
 
     //Forma 2
+    /*
     promesa.then(response => response.json()).then(data=>{
       this.setState({
         articles: data
       });
+    });
+  */
+
+    //Forma 3 async await
+    const posts = await promesa.then(response => response.json());
+    console.log(posts);
+    
+    let promesa2 = fetch("https://jsonplaceholder.typicode.com/posts?userId=1");
+    const posts2 = await promesa2.then(response => response.json());
+    console.log(posts2);
+
+
+    this.setState({
+      articles: posts
     });
   }
 
   render() {
     return (
       <div>
-       {
-        this.state.articles.map((article)=>{
-          return <div className="card" key={article.id.toString()}>
-            <p>
-              <b>{article.title}</b>
-            </p>
-            <p>
-              {article.body}
-            </p>
-            <Count /> 
-          </div>
-        })
-      }
+        {this.state.articles.map(article => {
+          return (
+            <div className="card" key={article.id.toString()}>
+              <p>
+                <b>{article.title}</b>
+              </p>
+              <p>{article.body}</p>
+              <Count />
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
-render(<Blog />, document.getElementById('root'));
+render(<Blog />, document.getElementById("root"));
